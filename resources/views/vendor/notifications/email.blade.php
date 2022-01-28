1b296581a7862e9b91183ed4dcf2deb5a54fd137
toya <toya24xxx@i.softbank.jp>
2022-01-28 19:38:43 +0900

    メール認証の日本語化

---
+++ resources/views/vendor/notifications/email.blade.php
@@ -0,0 +1,60 @@
+@component('mail::message')
+{{-- Greeting --}}
+@if (! empty($greeting))
+# {{ $greeting }}
+@else
+@if ($level === 'error')
+# @lang('Whoops!')
+@else
+# @lang('Thank you.')
+@endif
+@endif
+
+{{-- Intro Lines --}}
+@foreach ($introLines as $line)
+{{ $line }}
+
+@endforeach
+
+{{-- Action Button --}}
+@isset($actionText)
+<?php
+switch ($level) {
+    case 'success':
+    case 'error':
+        $color = $level;
+        break;
+    default:
+        $color = 'primary';
+}
+?>
+@component('mail::button', ['url' => $actionUrl, 'color' => $color])
+{{ $actionText }}
+@endcomponent
+@endisset
+
+{{-- Outro Lines --}}
+@foreach ($outroLines as $line)
+{{ $line }}
+
+@endforeach
+
+{{-- Salutation --}}
+@if (! empty($salutation))
+{{ $salutation }}
+@endif
+
+{{-- Subcopy --}}
+@isset($actionText)
+@slot('subcopy')
+@lang(
+"上記のボタンが正常に動かない場合は、こちらのURLをコピーし、ブラウザでお開き下さい\n".
+': [:actionURL](:actionURL)',
+[
+'actionText' => $actionText,
+'actionURL' => $actionUrl,
+]
+)
+@endslot
+@endisset
+@endcomponent
